cc994a5301aca6f7bd04bd0236ccfe415f433e20
msi-end <aditya01377@gmail.com>
2025-05-17 10:30:07 +0530

    new updates of folder and files

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://jagya.reynrel.in',
+    'https://jagya.reynrel.in'
 ];
 
 module.exports = allowedOrigins;
